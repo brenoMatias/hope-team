ca8595edcf479cf0300ffcaece76333c9e9cc04f
brenoMatias <matiasbreno19@gmail.com>
2022-02-09 20:13:01 -0300

    fixing mobile

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 
 export default function teste() {
     return (
 
-            <div> 
-                <h1> teste</h1>
+            <div className='about'> 
+                <div className="about-main">
+                <div className='about-text'> 
+                
+                <h1> Dudu e a Hope Team teste</h1>
+                    <p> Tudo começou no interior da Bahia, na cidade Jacobina, onde nasceu Luis Eduardo Souza, o Dudu. Ainda criança, Dudu foi morar na capital Salvador. Aos 12 anos, em um projeto social de um amigo, Dudu conheceu o boxe e se apaixonou pelo esporte. O boxe começou como uma forma de se defender na escola, mas logo se tornou uma oportunidade de mudança de vida e conquistas.</p>
+
+                    <p>
+                    Se inspirando em seus conterrâneos baianos em ascensão no boxe, Dudu seguiu os passos dos campeões e se tornou profissional em 2010. Com seu esforço, dedicação e profissionalismo, o boxeador também teve oportunidade de mostrar seu talento em países como Uruguai, Portugal e Espanha. Com DNA de lutador, Dudu tem também no sangue a vontade e o prazer de ensinar.
+                    </p>
+                    <p>
+                    Em 2012 a Hope Team foi em Ipatinga/MG com intuito de transformar a vida de pessoas que buscam uma paixão esportiva. A Hope serve a diversos lutadores amadores e também profissionais. Aqueles também que buscam apenas por uma atividade física divertida e de resultados também são muito bem vindos.
+                    </p>
+                    <p>
+                    O boxe trás consigo diversos beníficios corporais e mentais. A Hope oferece aulas de boxe em turmas e personal trainer para homens e mulheres de todas as idades. Oferecemos aulas experimentais para aqueles que querem conhecer o treino. Venha mudar seu estilo de vida!
+                    </p>
+
+
+                    <h1>Projeto Social </h1>
+
+                    <p>Com uma carreira de muitas vitórias, Dudu sentiu a necessidade de retribuir a oportunidade que lhe foi dada na infância. Seguindo os pensamentos do projeto social que o acolheu, o boxeador criou o projeto Dudu Nova Geração para atender crianças carentes. </p>
+                    <p> 
+                    Além de ser um espaço para crianças se encontrarem no esporte e absorvervalores para a vida, o projeto Dudu Nova Geração também gerou novos profissionais do boxe e MMA. Os ensinamentos da cultura da arte marcial e ajuda ao próximo tem sido a base de muitos.
+                    </p>
+                    <p >
+                    Com fé, esperança e força para lutar pelos sonhos a história é criada!
+                    </p>
+
+
+                </div>
+
+                </div>
+                <Footer />
             </div>
+            
             
     )
 }
