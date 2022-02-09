e5939645790b223d6819277f3776bae31f32b787
brenoMatias <matiasbreno19@gmail.com>
2022-02-09 13:38:16 -0300

    improving details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,15 +10,18 @@ function About() {
             <div className="about"> 
                 <div className="about-main">
                     <div className='about-text'> 
-                    <h1 className='h-about'>Luis Eduardo Souza - Dudu</h1>
+                    <h1 className='h-about'> Dudu e a Hope Team</h1>
                     <div className='p-box'> 
-                    <p className='p-about'> Tudo começou no interior da Bahia, na cidade Jacobina, mas, ainda criança, Dudu foi morar na capital Salvador. Aos 12 anos, em um projeto social de um amigo, Dudu conheceu o boxe e se apaixonou pelo esporte. O boxe começou como uma forma de se defender na escola, mas logo se tornou uma oportunidade de mudança de vida e conquistas.</p>
+                    <p className='p-about'> Tudo começou no interior da Bahia, na cidade Jacobina, onde nasceu Luis Eduardo Souza, o Dudu. Ainda criança, Dudu foi morar na capital Salvador. Aos 12 anos, em um projeto social de um amigo, Dudu conheceu o boxe e se apaixonou pelo esporte. O boxe começou como uma forma de se defender na escola, mas logo se tornou uma oportunidade de mudança de vida e conquistas.</p>
 
                     <p className='p-about'>
-                    Se inspirando em seus conterrâneos baianos em ascensão no boxe, Dudu seguiu os passos dos campeões e se tornou profissional em 2010. Com seu esforço, dedicação e profissionalismo, o boxeador também teve oportunidade de mostrar seu talento em países como Uruguai, Portugal e Espanha.
+                    Se inspirando em seus conterrâneos baianos em ascensão no boxe, Dudu seguiu os passos dos campeões e se tornou profissional em 2010. Com seu esforço, dedicação e profissionalismo, o boxeador também teve oportunidade de mostrar seu talento em países como Uruguai, Portugal e Espanha. Com DNA de lutador, Dudu tem também no sangue a vontade e o prazer de ensinar.
                     </p>
                     <p className='p-about'>
-                    Com DNA de lutador, Dudu tem também no sangue a vontade e o prazer de ensinar. Em 2012 fundou a Hope Team em Ipatinga/MG com intuito de transformar a vida de pessoas que buscam uma paixão esportiva.
+                    Em 2012 a Hope Team foi fundada em Ipatinga/MG com intuito de transformar a vida de pessoas que buscam uma paixão esportiva. A Hope serve a diversos lutadores amadores e também profissionais. Aqueles também que buscam apenas por uma atividade física divertida e de resultados também são muito bem vindos.
+                    </p>
+                    <p className='p-about'>
+                    O boxe trás consigo diversos beníficios corporais e mentais. A Hope oferece aulas de boxe em turmas e personal trainer para homens e mulheres de todas as idades. Oferecemos aulas experimentais para aqueles que querem conhecer o treino. Venha mudar seu estilo de vida!
                     </p>
 
 
