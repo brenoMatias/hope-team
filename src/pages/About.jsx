7a75b179f3589b550bb60559756b5350b0eae904
brenoMatias <matiasbreno19@gmail.com>
2022-04-11 12:39:18 -0300

    starting final updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import '../App.css';
 import Footer from '../components/Footer';
-import crop2 from '../images/crop2.svg';
 import hope from '../images/hope.jpeg';
+import ReactPlayer from 'react-player'
+
+
+
 
 function About() {
     return (
@@ -16,7 +19,7 @@ function About() {
                     Se inspirando em seus conterrâneos baianos em ascensão no boxe, Dudu seguiu os passos dos campeões e se tornou profissional em 2010. Com seu esforço, dedicação e profissionalismo, o boxeador também teve oportunidade de mostrar seu talento em países como Uruguai, Portugal e Espanha. Com DNA de lutador, Dudu tem também no sangue a vontade e o prazer de ensinar.
                     </p>
                     <p className='p-about'>
-                    Em 2012 a Hope Team foi em Ipatinga/MG com intuito de transformar a vida de pessoas que buscam uma paixão esportiva. A Hope serve a diversos lutadores amadores e também profissionais. Aqueles também que buscam apenas por uma atividade física divertida e de resultados também são muito bem vindos.
+                    Em 2009 a Hope Team foi fundada em Ipatinga/MG com intuito de transformar a vida de pessoas que buscam uma paixão esportiva. A Hope serve a diversos lutadores amadores e também profissionais. Aqueles também que buscam apenas por uma atividade física divertida e de resultados também são muito bem vindos.
                     </p>
                     <p className='p-about'>
                     O boxe trás consigo diversos beníficios corporais e mentais. A Hope oferece aulas de boxe em turmas e personal trainer para homens e mulheres de todas as idades. Oferecemos aulas experimentais para aqueles que querem conhecer o treino. Venha mudar seu estilo de vidadsadadaa!
@@ -26,11 +29,21 @@ function About() {
                    target='_blank' rel="noreferrer">
                     <p>Como chegar</p>
                     </a>
-
                      </p> 
+                    
+            <div className='player-wrapper'>
+                    <ReactPlayer
+                    className='react-player fixed-bottom'
+                    url= {}
+                    width='100%'
+                    height='100%'
+                    controls = {true}
+                     />
+                </div>
+             
 
                     <h1 className='h-about'>Projeto Social </h1>
-                    <p className='p-about'>Com uma carreira de muitas vitórias, Dudu sentiu a necessidade de retribuir a oportunidade que lhe foi dada na infância. Seguindo os pensamentos do projeto social que o acolheu, o boxeador criou o projeto Dudu Nova Geração para atender crianças carentes. </p>
+                    <p className='p-about'>Com uma carreira de muitas vitórias, Dudu sentiu a necessidade de retribuir a oportunidade que lhe foi dada na infância. Seguindo os pensamentos do projeto social que o acolheu, o boxeador criou seu próprio projeto para atender crianças carentes. Então em 2010 surgiu o projeto Dudu Nova Geração </p>
                     <p className='p-about'> 
                     Além de ser um espaço para crianças se encontrarem no esporte e absorvervalores para a vida, o projeto Dudu Nova Geração também gerou novos profissionais do boxe e MMA. Os ensinamentos da cultura da arte marcial e ajuda ao próximo tem sido a base de muitos.
                     </p>
